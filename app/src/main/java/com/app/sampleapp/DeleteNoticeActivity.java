4536be349e25d9bcde4fe7604d2398e173ea369d
Aalok Sah <aalok.sah25@gmail.com>
2022-12-06 16:58:20 +0545

    added layout for delete notice in admin app

---
+++ app/src/main/java/com/app/sampleapp/DeleteNoticeActivity.java
@@ -0,0 +1,14 @@
+package com.app.sampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_notice);
+    }
+}
