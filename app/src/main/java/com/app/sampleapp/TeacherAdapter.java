a29db54e1e7b852317bf3eac87e063d87694ae89
Aalok Sah <aalok.sah25@gmail.com>
2022-12-06 16:57:24 +0545

    added code for update teachers

--- app/src/main/java/com/app/sampleapp/TeacherAdapter.java
+++ app/src/main/java/com/app/sampleapp/TeacherAdapter.java
@@ -1,6 +1,7 @@
 package com.app.sampleapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,10 +20,12 @@ import java.util.List;
 public class TeacherAdapter extends RecyclerView.Adapter<TeacherAdapter.TeacherViewAdapter> {
     private List<TeacherData> list;
     private Context context;
+    private String category;
 
-    public TeacherAdapter(List<TeacherData> list, Context context) {
+    public TeacherAdapter(List<TeacherData> list, Context context, String category) {
         this.list = list;
         this.context = context;
+        this.category = category;
     }
 
     @NonNull
@@ -48,7 +51,14 @@ public class TeacherAdapter extends RecyclerView.Adapter<TeacherAdapter.TeacherV
         holder.update.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(context, "Update Teacher", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,UpdateTeacherActivity.class);
+                intent.putExtra("name",item.getName());
+                intent.putExtra("email",item.getEmail());
+                intent.putExtra("post",item.getPost());
+                intent.putExtra("image",item.getImage());
+                intent.putExtra("key",item.getKey());
+                intent.putExtra("category", category);
+                context.startActivity(intent);
             }
         });
 
--- app/src/main/java/com/app/sampleapp/UpdateFaculty.java
+++ app/src/main/java/com/app/sampleapp/UpdateFaculty.java
@@ -83,7 +83,7 @@ public class UpdateFaculty extends AppCompatActivity {
                     }
                     csDepartment.setHasFixedSize(true);
                     csDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
-                    adapter = new TeacherAdapter(list1,UpdateFaculty.this);
+                    adapter = new TeacherAdapter(list1,UpdateFaculty.this, "Computer Science");
                     csDepartment.setAdapter(adapter);
                 }
 
@@ -116,7 +116,7 @@ public class UpdateFaculty extends AppCompatActivity {
                     }
                     mechanicalDepartment.setHasFixedSize(true);
                     mechanicalDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
-                    adapter = new TeacherAdapter(list2,UpdateFaculty.this);
+                    adapter = new TeacherAdapter(list2,UpdateFaculty.this, "Mechanical");
                     mechanicalDepartment.setAdapter(adapter);
                 }
 
@@ -149,7 +149,7 @@ public class UpdateFaculty extends AppCompatActivity {
                     }
                     physicsDepartment.setHasFixedSize(true);
                     physicsDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
-                    adapter = new TeacherAdapter(list3,UpdateFaculty.this);
+                    adapter = new TeacherAdapter(list3,UpdateFaculty.this, "Physics");
                     physicsDepartment.setAdapter(adapter);
                 }
 
@@ -182,7 +182,7 @@ public class UpdateFaculty extends AppCompatActivity {
                     }
                     chemistryDepartment.setHasFixedSize(true);
                     chemistryDepartment.setLayoutManager(new LinearLayoutManager(UpdateFaculty.this));
-                    adapter = new TeacherAdapter(list4,UpdateFaculty.this);
+                    adapter = new TeacherAdapter(list4,UpdateFaculty.this, "Chemistry");
                     chemistryDepartment.setAdapter(adapter);
                 }
 
