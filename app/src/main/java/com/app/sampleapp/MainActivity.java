1a39bb3b1aef16293d688a64b5f3282b2f011ff4
Aalok Sah <aalok.sah25@gmail.com>
2023-01-08 09:57:24 +0545

    added code for delete notice

--- app/src/main/java/com/app/sampleapp/MainActivity.java
+++ app/src/main/java/com/app/sampleapp/MainActivity.java
@@ -7,9 +7,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    CardView uploadNotice, addGalleryImage, addEbook, faculty;
+    CardView uploadNotice, addGalleryImage, addEbook, faculty, deleteNotice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,33 +20,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         addGalleryImage = findViewById(R.id.addGalleryImage);
         addEbook = findViewById(R.id.addEbook);
         faculty = findViewById(R.id.faculty);
-
+        deleteNotice = findViewById(R.id.deleteNotice);
 
 
         uploadNotice.setOnClickListener(this);
         addGalleryImage.setOnClickListener(this);
         addEbook.setOnClickListener(this);
         faculty.setOnClickListener(this);
+        deleteNotice.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         Intent intent;
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.addNotice:
-                intent = new Intent(MainActivity.this,UploadNotice.class);
+                intent = new Intent(MainActivity.this, UploadNotice.class);
                 startActivity(intent);
                 break;
             case R.id.addGalleryImage:
-                intent = new Intent(MainActivity.this,UploadImage.class);
+                intent = new Intent(MainActivity.this, UploadImage.class);
                 startActivity(intent);
                 break;
             case R.id.addEbook:
-                intent = new Intent(MainActivity.this,UploadPdfActivity.class);
+                intent = new Intent(MainActivity.this, UploadPdfActivity.class);
                 startActivity(intent);
                 break;
             case R.id.faculty:
                 intent = new Intent(MainActivity.this, UpdateFaculty.class);
+                startActivity(intent);
+                break;
+            case R.id.deleteNotice:
+                intent = new Intent(MainActivity.this, DeleteNoticeActivity.class);
                 startActivity(intent);
                 break;
         }
